a8383279d668c609e1deaf0e7e674869dc5262e1
ramosvi <vitor_hugo_di@hotmail.com>
2024-03-29 14:30:23 -0300

    feat: cria padrão Registry com Singleton e Proxy

--- backend/src/infra/controller/MainController.ts
+++ backend/src/infra/controller/MainController.ts
@@ -1,21 +1,26 @@
-import AccountRepositoryDatabase from "../repository/AccountRepositoryDatabase";
 import GetAccount from "../../application/usecase/GetAccount";
 import HttpServer from "../http/HttpServer";
-import LoggerConsole from "../logger/LoggerConsole";
-import PgPromiseAdapter from "../database/PgPromiseAdapter";
 import Signup, { Input } from "../../application/usecase/Signup";
+import { inject } from "../di/Registry";
 
 // Interface Adapter
 export default class MainController {
+	// Pattern Decorator
+	@inject("httpServer")
+	httpServer?: HttpServer;
+	@inject("signup")
+	signup?: Signup;
+	@inject("getAccount")
+	getAccount?: GetAccount
 
-	constructor (readonly httpServer: HttpServer, signup: Signup, getAccount: GetAccount) {
-		httpServer.register("post", "/signup", async function (params: any, body: any) {
-			const output = await signup.execute(body);
+	constructor () {
+		this.httpServer?.register("post", "/signup", async (params: any, body: any) => {
+			const output = await this.signup?.execute(body);
 			return output;
 		});
 
-		httpServer.register("get", "/accounts/:accountId", async function (params: any, body: any) {
-			const output = await getAccount.execute(params.accountId);
+		this.httpServer?.register("get", "/accounts/:accountId", async (params: any, body: any) => {
+			const output = await this.getAccount?.execute(params.accountId);
 			return output;
 		})
 	}
